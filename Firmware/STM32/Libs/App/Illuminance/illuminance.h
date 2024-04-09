de5f7265fa3c3398a656f45087f65ec54e81e722
HuynPann511 <158193529+HuynPann511@users.noreply.github.com>
2024-04-09 18:21:06 +0700

    Update illuminance.h

--- Firmware/STM32/Libs/App/Illuminance/illuminance.h
+++ Firmware/STM32/Libs/App/Illuminance/illuminance.h
@@ -8,6 +8,10 @@
 #ifndef APP_ILLUMINANCE_ILLUMINANCE_H_
 #define APP_ILLUMINANCE_ILLUMINANCE_H_
 
+/***********************************************************/
 
+float illumi_adc (float y);
+
+/***********************************************************/
 
 #endif /* APP_ILLUMINANCE_ILLUMINANCE_H_ */
