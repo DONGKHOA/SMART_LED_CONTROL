9ed5893f97768a95da5ca0b7a4fb071b4535ea1d
HuynPann511 <158193529+HuynPann511@users.noreply.github.com>
2024-04-09 18:22:37 +0700

    Update read_adc.c

--- Firmware/STM32/Libs/Driver/ADC/read_adc.c
+++ Firmware/STM32/Libs/Driver/ADC/read_adc.c
@@ -4,5 +4,16 @@
  *  Created on: Apr 9, 2024
  *      Author: dongkhoa
  */
+#include "read_adc.h"
+#include "stm32f1xx_hal.h"
 
+extern ADC_HandleTypeDef hadc1;
 
+int read_adc (void)
+{
+    HAL_ADC_Start(&hadc1);
+	  HAL_Delay(50);
+	  int16_t var = HAL_ADC_GetValue(&hadc1);
+    HAL_ADC_Stop(&hadc1);
+    return var;
+}
