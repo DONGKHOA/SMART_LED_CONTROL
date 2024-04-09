e2edc3249ac4160e31b87277c1b8d0b849a1d1a7
HuynPann511 <158193529+HuynPann511@users.noreply.github.com>
2024-04-09 18:24:27 +0700

    Update calibrate_adc.c

--- Firmware/STM32/Libs/Middleware/Calibrate_ADC/calibrate_adc.c
+++ Firmware/STM32/Libs/Middleware/Calibrate_ADC/calibrate_adc.c
@@ -4,5 +4,10 @@
  *  Created on: Apr 9, 2024
  *      Author: dongkhoa
  */
+#include "calibrate_adc.h"
 
-
+int calibrate_adc(int var)
+ {
+	  int y = (float)var* 0.9769 + 157.13;
+    return y;
+ }
