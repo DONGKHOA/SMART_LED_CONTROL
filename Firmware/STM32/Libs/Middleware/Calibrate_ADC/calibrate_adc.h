f03ae8bd56e008b1b50533a97b609c2eb15c0f73
HuynPann511 <158193529+HuynPann511@users.noreply.github.com>
2024-04-09 18:23:52 +0700

    Update calibrate_adc.h

--- Firmware/STM32/Libs/Middleware/Calibrate_ADC/calibrate_adc.h
+++ Firmware/STM32/Libs/Middleware/Calibrate_ADC/calibrate_adc.h
@@ -7,7 +7,7 @@
 
 #ifndef MIDDLEWARE_CALIBRATE_ADC_CALIBRATE_ADC_H_
 #define MIDDLEWARE_CALIBRATE_ADC_CALIBRATE_ADC_H_
-
-
-
+/**************************************************/
+int calibrate_adc(int);
+/**************************************************/
 #endif /* MIDDLEWARE_CALIBRATE_ADC_CALIBRATE_ADC_H_ */
