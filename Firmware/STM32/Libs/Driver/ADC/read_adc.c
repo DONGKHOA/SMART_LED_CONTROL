2dfa9ce8292a387c303d06cd9f7d50cc2c1cc729
HuynPann511 <158193529+HuynPann511@users.noreply.github.com>
2024-04-09 23:29:41 +0700

    Update read_adc.c

--- Firmware/STM32/Libs/Driver/ADC/read_adc.c
+++ Firmware/STM32/Libs/Driver/ADC/read_adc.c
@@ -7,14 +7,16 @@
 #include "read_adc.h"
 #include "stm32f1xx_hal.h"
 #include "stdint.h"
+#include "FreeRTOS.h"
+#include "task.h"
 
 extern ADC_HandleTypeDef hadc1;
 
 int16_t read_adc (void)
 {
-    	HAL_ADC_Start(&hadc1);
-	HAL_Delay(50);
+    HAL_ADC_Start(&hadc1);
+	vTaskDelay(50);
 	int16_t var = HAL_ADC_GetValue(&hadc1);
-    	HAL_ADC_Stop(&hadc1);
-    	return var;
+    HAL_ADC_Stop(&hadc1);
+    return var;
 }
