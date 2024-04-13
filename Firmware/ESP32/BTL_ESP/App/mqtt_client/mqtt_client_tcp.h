ca131c28b39cfb603d2933c39810269b085f4391
DONGKHOA <thanhkhoa2k3@gmail.com>
2024-04-13 11:16:59 +0700

    update:esp32: name project

---
+++ Firmware/ESP32/BTL_ESP/App/mqtt_client/mqtt_client_tcp.h
@@ -0,0 +1,29 @@
+#ifndef MQTT_CLIENT_H_
+#define MQTT_CLIENT_H_
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+#include <stdint.h>
+#include "mqtt_client.h"
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+typedef struct 
+{
+    char data [10];
+    esp_mqtt_client_handle_t client;
+}MQTT_Client_Data_t;
+
+/**********************
+ *   GLOBAL FUNCTIONS
+ **********************/
+
+void MQTT_app_start(MQTT_Client_Data_t *MQTT_Client, char *url);
+int8_t MQTT_app_get_data(MQTT_Client_Data_t *MQTT_Client);
+int8_t MQTT_app_state_connect(void);
+
+#endif
