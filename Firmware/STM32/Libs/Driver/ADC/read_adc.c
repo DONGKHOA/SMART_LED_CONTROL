525f5ad64fe402064e754a4288830bfb3db0c96d
HuynPann511 <158193529+HuynPann511@users.noreply.github.com>
2024-04-15 00:00:22 +0700

    Update read_adc.c

--- Firmware/STM32/Libs/Driver/ADC/read_adc.c
+++ Firmware/STM32/Libs/Driver/ADC/read_adc.c
@@ -9,11 +9,11 @@
 #include "task.h"
 
 
-int16_t read_adc(ADC_HandleTypeDef hadc1)
+int16_t read_adc(ADC_HandleTypeDef hadc2)
 {
-    HAL_ADC_Start(&hadc1);
+    HAL_ADC_Start(&hadc2);
     vTaskDelay(50);
-    int16_t var = HAL_ADC_GetValue(&hadc1);
-    HAL_ADC_Stop(&hadc1);
+    int16_t var = HAL_ADC_GetValue(&hadc2);
+    HAL_ADC_Stop(&hadc2);
     return var;
 }
