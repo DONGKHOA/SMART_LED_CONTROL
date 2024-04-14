380788ddc4c9574f1a7fdb03852064698bf5e902
HuynPann511 <158193529+HuynPann511@users.noreply.github.com>
2024-04-15 00:00:36 +0700

    Update read_adc.h

--- Firmware/STM32/Libs/Driver/ADC/read_adc.h
+++ Firmware/STM32/Libs/Driver/ADC/read_adc.h
@@ -13,7 +13,7 @@
 #include "stm32f1xx_hal.h"
 #include "stdint.h"
 
-int16_t read_adc(ADC_HandleTypeDef hadc1);
+int16_t read_adc(ADC_HandleTypeDef hadc2);
 
 /***********************************************************************/
 
