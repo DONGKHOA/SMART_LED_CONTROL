e18fd617e2ab6e6712fb8400034cc178089b1bdf
HuynPann511 <158193529+HuynPann511@users.noreply.github.com>
2024-04-15 18:48:47 +0700

    Update cal_temperature.c

--- Firmware/STM32/Libs/App/Temperature/cal_temperature.c
+++ Firmware/STM32/Libs/App/Temperature/cal_temperature.c
@@ -1,14 +1,11 @@
+
 #include "cal_temperature.h"
 #include "stdint.h"
 #include "calibrate_adc.h"
 
-ADC_HandleTypeDef hadc1;
-DMA_HandleTypeDef hdma_adc1;
-TIM_HandleTypeDef htim3;
-UART_HandleTypeDef huart1;
 
 uint8_t UpdateEvent;
-uint16_t ADC_RESULT[2];
+extern uint16_t ADC_RESULT[2];
 float V_Sense, V_Ref;
 
 
@@ -17,12 +14,6 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
     // Xử lý khi quá trình chuyển đổi ADC hoàn thành
 	HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
 	UpdateEvent = 1;
-}
-
-void temperature_sensor_init(void)
-{
-	 HAL_TIM_Base_Start(&htim3); // Start Timer3 (Trigger Source For ADC1)
-	 HAL_ADC_Start_DMA(&hadc1, (uint32_t*)ADC_RESULT, 2);   // Start ADC Conversion
 }
 
 float calculate_temperature(void)
@@ -36,3 +27,4 @@ float calculate_temperature(void)
 	return Temperature;
 	UpdateEvent = 0;
 }
+
