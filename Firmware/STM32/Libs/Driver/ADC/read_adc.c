1df94403ca22fd7e8a8aad7d5b418b5c2813d35c
HuynPann511 <158193529+HuynPann511@users.noreply.github.com>
2024-04-15 18:45:12 +0700

    Update read_adc.c

--- Firmware/STM32/Libs/Driver/ADC/read_adc.c
+++ Firmware/STM32/Libs/Driver/ADC/read_adc.c
@@ -8,12 +8,18 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+uint16_t ADC_RESULT[2];
 
-int16_t read_adc(ADC_HandleTypeDef hadc2)
+int16_t read_adc (ADC_HandleTypeDef* hadc)
 {
-    HAL_ADC_Start(&hadc2);
+    HAL_ADC_Start(hadc);
     vTaskDelay(50);
-    int16_t var = HAL_ADC_GetValue(&hadc2);
-    HAL_ADC_Stop(&hadc2);
+    int16_t var = HAL_ADC_GetValue(hadc);
+    HAL_ADC_Stop(hadc);
     return var;
+}
+void temperature_sensor_init(ADC_HandleTypeDef* hadc, TIM_HandleTypeDef* htim)
+{
+	 HAL_TIM_Base_Start(htim); // Start Timer3 (Trigger Source For ADC1)
+	 HAL_ADC_Start_DMA(hadc, (uint32_t*)ADC_RESULT, 2);   // Start ADC Conversion
 }
