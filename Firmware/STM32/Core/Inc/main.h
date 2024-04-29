e62ffbb062db416ca9ccfaa59e74b15bd52f2d25
HuynPann511 <158193529+HuynPann511@users.noreply.github.com>
2024-04-29 14:18:41 +0700

    Move include lib from main.c to main.h

--- Firmware/STM32/Core/Inc/main.h
+++ Firmware/STM32/Core/Inc/main.h
@@ -31,7 +31,8 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "string.h"
+#include "uartstdio.h"
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
